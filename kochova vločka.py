ad525ca76f5227c00fc59a77deb994eea0d04da8
Viola Kejřová <88209579+violakejrova@users.noreply.github.com>
2021-11-22 18:48:32 +0100

    Kochova vlocka

---
+++ kochova vločka.py
@@ -0,0 +1,43 @@
+import turtle
+
+
+rada = ['F++F++F']
+
+pravidla = {'F': 'F+F--F+F'}
+
+opakovani = int (input('počet generací: '))
+
+def lsystem (rada, pravidla, opakovani):
+  for _ in range (opakovani):
+    x = []
+    for pismenko in rada:
+      if pismenko in pravidla:
+        x += pravidla[pismenko]
+      else:
+        x += pismenko
+    rada = x
+  return rada
+
+    
+for q in range (opakovani+1):
+  print('generace {}: {}'.format(q, lsystem(rada,pravidla, q)))
+
+w = turtle.Screen()
+#w.bgcolor('black')
+w.title('Kochova vločka')
+w.screensize()
+w.setup(width = 1.0, height = 1.0)
+turtle.Turtle()
+#turtle.color('white')
+r = lsystem(rada, pravidla, opakovani)
+  
+
+turtle.speed(1000)
+
+for pismenko in r:
+  if pismenko == 'F':
+     turtle.forward(5)
+  elif pismenko == '-':
+     turtle.right(60)
+  elif pismenko == '+':
+      turtle.left(60)
